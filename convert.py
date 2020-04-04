412dbddb6d3c2629f3d4dd69071f59ed748f2bf5
Nick Samuel <nicksamuel@users.noreply.github.com>
2020-04-04 16:58:11 +0100

    convert.py
    Python Script for PDFtables.com API

---
+++ convert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import pdftables_api
+import os
+import requests
+
+#Nick defining variables
+
+api = "abcdefghijkl"
+request = "https://pdftables.com/api/remaining?key="
+query = request+api
+
+# stuff I didn't do
+
+c = pdftables_api.Client(api)
+
+file_path = "/home/username/wherever-you-save-this-script"
+
+for file in os.listdir(file_path):
+    if file.endswith(".PDF"):
+        c.xlsx(os.path.join(file_path,file), file+'.xlsx')
+
+# Nick add on for credits check to terminal
+credz = requests.get(query)
+
+print(credz.text)
+
+input("Press enter to exit ;)")
