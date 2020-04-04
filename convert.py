522157d5a359d2151d59c154ce41d4ffaea20e20
Nick Samuel <nicksamuel@users.noreply.github.com>
2020-04-04 17:04:24 +0100

    Update convert.py

--- convert.py
+++ convert.py
@@ -2,13 +2,13 @@ import pdftables_api
 import os
 import requests
 
-#Nick defining variables
+# Nick Variables - Insert your API key here from https://pdftables.com/pdf-to-excel-api
 
 api = "abcdefghijkl"
 request = "https://pdftables.com/api/remaining?key="
 query = request+api
 
-# stuff I didn't do
+# Stuff I didn't do. Note script looks for PDFs to convert relative to where it runs i.e. this script should be saved in the same folder as the PDFs you want to convert.
 
 c = pdftables_api.Client(api)
 
@@ -18,7 +18,7 @@ for file in os.listdir(file_path):
     if file.endswith(".PDF"):
         c.xlsx(os.path.join(file_path,file), file+'.xlsx')
 
-# Nick add on for credits check to terminal
-credz = requests.get(query)
+# Nick API Credits - This will print the remaining API credits in terminal or Python Console after conversion. I needed this badly and worked out a quick way of doing it.
+credits = requests.get(query)
 
-print(credz.text)
+print(credits.text)
