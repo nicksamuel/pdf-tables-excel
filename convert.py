370dd03cc09f7d474c17fadb0339e52e9db1415b
Nick Samuel <nicksamuel@users.noreply.github.com>
2020-04-04 17:06:51 +0100

    Update convert.py

--- convert.py
+++ convert.py
@@ -8,7 +8,7 @@ api = "abcdefghijkl"
 request = "https://pdftables.com/api/remaining?key="
 query = request+api
 
-# Stuff I didn't do. Note script looks for PDFs to convert relative to where it runs i.e. this script should be saved in the same folder as the PDFs you want to convert.
+# Stuff I didn't do. Note script looks for PDFs to convert relative to where it runs i.e. this script should be saved in the same folder as the PDFs you want to convert
 
 c = pdftables_api.Client(api)
 
@@ -18,7 +18,7 @@ for file in os.listdir(file_path):
     if file.endswith(".PDF"):
         c.xlsx(os.path.join(file_path,file), file+'.xlsx')
 
-# Nick API Credits - This will print the remaining API credits in terminal or Python Console after conversion. I needed this badly and worked out a quick way of doing it.
+# Nick API Credits - This will print the remaining API credits in terminal or Python Console after conversion. I needed to know this and worked out a quick way of displaying the info
 credits = requests.get(query)
 
 print(credits.text)
