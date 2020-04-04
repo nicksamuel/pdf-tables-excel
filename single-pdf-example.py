47e12833644bafbd7dd827a4eba02f3af66911ce
Nick Samuel <nicksamuel@users.noreply.github.com>
2020-04-04 18:55:59 +0100

    Update single-pdf-example.py

--- single-pdf-example.py
+++ single-pdf-example.py
@@ -1,6 +1,4 @@
-# This is their example script for converting single PDF tables to excel or other formats: https://pdftables.com/blog/pdf-to-excel-with-python
-
-# As you can see it's fairly basic and involves quite a lot of manual work to get through more than one file...
+# This is their example script for converting single PDF tables to excel or other formats
 
 import pdftables_api
 
@@ -10,3 +8,5 @@ c.xlsx('input.pdf', 'output')
 #replace c.xlsx with c.csv to convert to CSV
 #replace c.xlsx with c.xml to convert to XML
 #replace c.xlsx with c.html to convert to HTML
+
+# As you can see it's fairly basic and involves quite a lot of manual work to convert multiple PDFs :-(
